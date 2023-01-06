c0e6bb961f66a40cff09b7a571a3ca64ae8c223d
gopi988530 <connect2vaka@gmail.com>
2023-01-05 19:05:33 -0800

    Initial Commit

---
+++ src/SwapTwoNum.java
@@ -0,0 +1,17 @@
+
+
+public class SwapTwoNum {
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+		System.out.println("Before swapping :" + a + "\t" + b);
+		a = a + b; // 30
+		b = a - b; // b=30-20=10
+		a = a - b; // a=30-10=20
+
+		System.out.println("After swapping :" + a + "\t" + b);
+
+	}
+
+}
